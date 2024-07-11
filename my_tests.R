e4e7de04b0004b6db9937de26edb7a226c462edb
Jacek Ostrowski <jacek.ostrowski@knowit.eu>
2024-07-11 09:39:51 +0200

    More examples

---
+++ my_tests.R
@@ -0,0 +1,12 @@
+library(dslabs)
+
+data(murders)
+
+class(murders)
+str(murders)
+summary(murders)
+class(murders$population)
+length(murders$population)
+
+class(murders$region)
+levels(murders$region)
