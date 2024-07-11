545a8c2cd9fb05a3170b4f0e40514cec90fa3ab7
Jacek Ostrowski <jacek.ostrowski@knowit.eu>
2024-07-11 12:03:36 +0200

    Finished Section 1 with quizzes

--- my_tests.R
+++ my_tests.R
@@ -10,3 +10,47 @@ length(murders$population)
 
 class(murders$region)
 levels(murders$region)
+
+
+# Quizzes
+
+# Quiz 1
+quadratic <- function(a, b, c) {
+  delta <- b^2 - 4*a*c
+  x1 <- (-b - sqrt(delta)) / (2*a)
+  x2 <- (-b + sqrt(delta)) / (2*a)
+  c(x1, x2)
+}
+quadratic(1, 1, -1)
+quadratic(2, -1, -4) # 1st question
+
+log(1024, base = 4)  # 2nd
+
+data(movielens)
+str(movielens) # 3a-3d, 4
+
+# Quiz 2
+# 1, 2
+x <- c(2, 43, 27, 96, 18)
+order(x)
+rank(x)
+sort(x)
+min(x)
+which.min(x)
+max(x)
+which.max(x)
+# 3
+name <- c("Mandi", "Amy", "Nicole", "Olivia")
+distance <- c(0.8, 3.1, 2.8, 4.0)
+time <- c(10, 30, 40, 50)
+time_hr <- time / 60
+distance / time_hr
+
+# 6,7,8
+data(olive)
+head(olive)
+str(olive)
+?olive
+plot(olive$palmitoleic, olive$palmitic)
+hist(olive$eicosenoic)
+boxplot(palmitic ~ region, data = olive)
