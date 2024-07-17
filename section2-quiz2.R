50cba7997df92bc0c822fb9f7fec9bc4cf0242c2
Jacek <notavailable@wherever.com>
2024-07-17 21:22:18 +0200

    Remove unneeded files

--- section2-quiz2.R
+++
@@ -1,8 +0,0 @@
-# question 8
-library(dslabs)
-data(murders)
-
-which(murders$state == "Massachusetts")
-match(c("Massachusetts"), murders$state)
-c("Massachusetts") %in% murders$state
-which(murders$state = "Massachusetts")
